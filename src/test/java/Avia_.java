03b2d5d01b1cf327aef6b9f1bdf6f9c7d798303b
Natalya <nnaa58@mail.ru>
2018-05-01 19:05:10 +0300

    ScreenShot added

--- src/test/java/Avia_.java
+++ src/test/java/Avia_.java
@@ -6,16 +6,22 @@ import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.internal.Locatable;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+
 
 public class Avia_ {
 
@@ -24,12 +30,11 @@ public class Avia_ {
         }
 
         public static WebDriver driver;
-        //Определяем первую страницу для входа  на сайт. Сразу страница для рейса Калуга - Прага
-        private static String baseurl = "https://avia.tutu.ru/offers/?passengers=100&changes=all&threeDays=false&route[0]=5731-12042018-376&class=Y";
 
+        //Определяем первую страницу для входа  на сайт. Сразу страница для рейса Калуга - Прага. Дату меняю вручную
+        private static String baseurl = "https://avia.tutu.ru/offers/?passengers=100&changes=all&threeDays=false&route[0]=5731-30082018-376&class=Y";
 
         @BeforeClass
-
         public static void setUp() throws Exception {
 
         driver = new ChromeDriver();
@@ -40,48 +45,81 @@ public class Avia_ {
     }
 
     @Test public void Head() throws IOException, InterruptedException, ATUTestRecorderException {
+
         //Начинаем видеозапись
             ATUTestRecorder recorder = new ATUTestRecorder("ScriptVideos", false);
         recorder.start();
-        FileWriter wri = new FileWriter("NaN.txt", true);
 
         WebDriverWait wait = new WebDriverWait(driver, 40);
-        //Задаем ожидания загрузки страницы
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "eager");
+
+
+//        //Задаем ожидания загрузки страницы
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability(CapabilityType.PAGE_LOAD_STRATEGY, "eager");
 
         //Идем на страницу входа
         driver.navigate().to(baseurl);
 
+
+        //Рейсы соседних городов
+        driver.findElement(By.cssSelector("li.service_item.m-near_cities")).click();
+
+        //Соседние города с Калугой и Прагой
         driver.findElement(By.xpath("//*[@id='near_cities']/div[5]/table/tbody/tr[1]/td[1]/div/div[1]/span")).click();
 
         //Прокручиваем страницу, чтобы была видна на экране последняя строка таблицы
         JavascriptExecutor js = ((JavascriptExecutor) driver);
         js.executeScript("window.scrollTo(0, 200);");
 
-        WebElement nearCities = driver.findElement(By.cssSelector("#near_cities > div:nth-child(5)"));
-        List<WebElement> prices = nearCities.findElements(By.cssSelector("div.j-price.j-direct_price"));
-        List<WebElement> pricesAll = nearCities.findElements(By.cssSelector("div.j-price.j-all_price"));
-        List<WebElement> rows = nearCities.findElements(By.cssSelector("tr.j-row.group_row"));
+        //Делаем скриншот, чтобы была видна таблица с багами
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File screenShotFile = new File("Screen_near_cities.png");
+        FileHandler.copy(scrFile, screenShotFile);
 
-        for (int i = 0; i < rows.size(); i++) {
+        //Проверяем содержимое полей, в которых указаны цены - должны содержать числа или "нет рейсов"
+        VerifyPrices();
+
+          recorder.stop();
+    }
+
+//    @After
+//    public void stop(){
+//        driver.quit();
+//        driver = null;
+//    }
+
+
+    private void VerifyPrices () throws IOException {
+
+        FileWriter wri = new FileWriter("NaN.txt", true);
+
+
+        //Таблица с рейсами и ценами
+        WebElement nearCities = driver.findElement(By.cssSelector("#near_cities > div:nth-child(5)"));
+
+        //Список строк из таблицы
+        List<WebElement> rows1 = nearCities.findElements(By.cssSelector("tr.j-row.group_row"));
+
+        //Проверяем цены в каждой строке
+        for (int i = 0; i < rows1.size(); i++) {
+            List<WebElement> pricesAll = nearCities.findElements(By.cssSelector("div.j-price.j-all_price"));
+            List<WebElement> rows = nearCities.findElements(By.cssSelector("tr.j-row.group_row"));
             String row = rows.get(i).getAttribute("outerText");
+            List<WebElement> prices = nearCities.findElements(By.cssSelector("div.j-price.j-direct_price"));
             String price1 = prices.get(i).getAttribute("outerText");
             String priceAll1 = pricesAll.get(i).getAttribute("outerText");
-            System.out.println(row);
+            wri.write(row);
 
             //Проверяем, что есть рейсы для данного маршрута
             if (price1.equals("нет рейсов") && priceAll1.equals("нет рейсов")) {
-                System.out.println("нет рейсов");
+                wri.write("Нет рейсов" + "\n");
             } else {
-
                 //С помощью регулярных выражений проверяем, что в поле с ценами содержатся цифры
                 String pattern = "([0-9])";
                 Pattern p = Pattern.compile(pattern);
                 String price2 = prices.get(i).getAttribute("outerText");
                 String priceAll2 = pricesAll.get(i).getAttribute("outerText");
 
-                System.out.println(price2);
                 Matcher mPrice = p.matcher(price2);
                 Matcher mPriceAll = p.matcher(priceAll1);
                 StringBuilder sbPrice = new StringBuilder();
@@ -98,18 +136,12 @@ public class Avia_ {
                     nomberAll = priceAll2.substring(mPriceAll.start(), mPriceAll.end());
                     sbPriceAll.append(nomberAll);
                 }
-                System.out.println("___" + sbPrice + "______" + sbPriceAll);
                 if (sbPrice.length() == 0 | sbPriceAll.length() == 0) {
-                    System.out.println("Price Error");
+                    wri.write("Price error__" + sbPrice + "______" + sbPriceAll + "\n");
                 }
-                System.out.println();
-                }
+            }
         }
-    }
-
-    @After
-    public void stop(){
-        driver.quit();
-        driver = null;
+        wri.flush();
+        wri.close();
     }
 }
